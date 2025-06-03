1c2dd76b2aa4a3b9842d65adcbbcc419ec8d7b2b
CaioBPQ <caiopompermayer@gmail.com>
2025-06-03 01:17:45 -0300

    Atualizar o script.js

--- dia dos namorados/script.js
+++ dia dos namorados/script.js
@@ -1,4 +1,4 @@
-const inicio = new Date("2024-10-05");
+const inicio = new Date("2024-10-04");
 const hoje = new Date();
 const diff = Math.floor((hoje - inicio) / (1000 * 60 * 60 * 24));
 document.getElementById("contador").innerText = `Estamos juntos há ${diff} dias ❤️`;
